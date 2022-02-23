8345b8c868603719794a6bb055661fd93c33439f
Juan Ramirez <rami8224.2007@gmail.com>
2022-02-23 15:10:42 -0500

    made functions

--- main.js
+++ main.js
@@ -1,16 +1,95 @@
 // ┌─────────────────┐
 // │ Your code here! │
 // └─────────────────┘
+//CREATE USER
+const createUser = function(first,last) {
+    //set empty user var to collect info
+    let user = {};
+    //set info to first/last
+    user['firstName']=first;
+    user['lastName']=last;
+    return user
+}
 
+// SET AGE
 
+const setAge = function(user, actualAge) {
+    //set age property w/ the actualAge as the value
+    user['age'] = actualAge;
+    return user;
+}
 
+//INCREMENT AGE
+const incrementAge =function(user) {
+    //access age prop and increase value by 1
+    user.age ++
+    return user
+}
 
+//FIX CAR
+const fixCar = function(car) {
+    car.needsMaintenance = false
+    return car
+}
 
+// ADD GRADES
+const addGrades = function(student, newGrades) {
+    //access student grades and concat newGrades to list
+    student.grades = student.grades.concat(newGrades)
+    return student
+}
 
+// GET DATA TYPE
+const getDataType = function(car,key) {
+    return typeof car[key]
+}
 
+//ADD TO-DO
+const addTodo = function(todo,newToDoItem) {
+    //directly add item to array
+    todo.push(newToDoItem)
+    return todo
+}
+//ADD SONG
+const addSong = function(list, track) {
+    //output:update durantion && add new track
+    //add song
+    list.songs.push(track);
+    //update duration of list by increasing it by last item
+    list.duration += list.songs[list.songs.length-1].duration
+    return list
+}
 
-
-
+//UPDATE REPORTCARD
+const updateReportCard = function(reportCard, newGrade) {
+    //add the newGrade
+    reportCard.grades.push(newGrade)
+    //get highest/lowest/avg grades
+    let highest = -Infinity
+    let lowest = Infinity
+    let avg  = getAvg(reportCard.grades)  
+    //check grades list and identify high/low/etc 
+    for(let i = 0 ; i < reportCard.grades.length; i++) {
+      if(highest < reportCard.grades[i] ) {
+        highest = reportCard.grades[i]
+      }
+      if (lowest > reportCard.grades[i]) {
+        lowest = reportCard.grades[i]
+      }
+    }
+    //update values in object
+    reportCard.lowestGrade = lowest;
+    reportCard.highestGrade = highest;
+    reportCard.averageGrade = avg
+    return reportCard
+}
+const getAvg = function(arr) {
+    let sum = 0
+    for(let num of arr) {
+        sum += num
+    }
+    return sum/arr.length
+}
 // ┌─────────────────────────────────────────────────────────────────────────┐
 // │ Code used for testing. Do not modify!                                   │                         
 // │                                                                         │
